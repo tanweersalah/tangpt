56bb386d5e64a58042a3df051c5443d860fb38ea
tanweersalah <tanweer.salah@gmail.com>
2024-09-06 12:16:24 +0200

    updated chat endpoint

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,15 +1,24 @@
+from typing import Annotated, Literal, TypedDict
 from fastapi import FastAPI
 import os
 from dotenv import load_dotenv
 from langchain_core.output_parsers import StrOutputParser
 from langchain_groq import ChatGroq
 from langserve import add_routes
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.indexes.vectorstore import VectorStoreIndexWrapper
+
+from langchain_community.vectorstores import FAISS
 from langchain_community.chat_message_histories import ChatMessageHistory
 from langchain_core.runnables.history import RunnableWithMessageHistory
 from langchain_core.messages import HumanMessage
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from fastapi.middleware.cors import CORSMiddleware
 
+from langgraph.graph import StateGraph, END, START
+
+from collections import OrderedDict
+import os
 load_dotenv()
 
 
@@ -34,19 +43,90 @@ model = ChatGroq(model="gemma-7b-it")
 
 parser = StrOutputParser()
 
-store = {}
+## inmemory chat history
+store = OrderedDict()
 
 def get_session_history(session_id):
+    ## Handle max 5 sessions
+    if len(store) > 4 :
+        store.popitem(last=False)
+    
     if session_id not in store :
-        store[session_id]= ChatMessageHistory()
+        store[session_id]= []
     return store[session_id]
 
 with_message_history = RunnableWithMessageHistory(model,get_session_history)
 
 chain = with_message_history|parser
 
-## App defination
+## DataClass
 
+class DecisionRouter(BaseModel):
+    decision : Literal['RAG', 'General'] = Field(..., description="Chose correct option based on user input, If User ask about Tanweer , Use RAG else General")
+
+
+
+## Vector DB Init
+embedding = OpenAIEmbeddings()
+db_path = os.path.join(os.path.dirname(__file__), "faiss_index")
+db = FAISS.load_local(db_path, embedding ,allow_dangerous_deserialization=True)
+
+vector_store_wrapper = VectorStoreIndexWrapper(vectorstore= db)
+
+##  LLMs
+
+router_llm = ChatGroq(model='llama-3.1-8b-instant')
+
+router_llm_structured = router_llm.with_structured_output(DecisionRouter)
+
+
+
+general_llm = ChatGroq(model='llama-3.1-8b-instant')
+
+## State
+
+from langgraph.graph.message import add_messages
+
+class AgentState(TypedDict):
+    messages: Annotated[list, add_messages]
+
+
+## Node Agents
+
+def input_router(state: AgentState):
+    decision = router_llm_structured.invoke(state['messages'])
+
+    return decision.decision
+
+
+
+def general_llm_agent(state : AgentState):
+
+    return {'messages' : [general_llm.invoke(state['messages'])] }
+
+
+def rag_query(state : AgentState):
+    print(state)
+    response = vector_store_wrapper.query(state['messages'][-1].content, general_llm)
+
+    return {'messages' :[response] }
+
+## Graph Builder
+
+from langgraph.graph import StateGraph, END, START
+
+workflow = StateGraph(AgentState)
+
+workflow.add_node('General' ,general_llm_agent)
+workflow.add_node('RAG', rag_query)
+
+workflow.add_conditional_edges(START,input_router )
+workflow.add_edge('General', END)
+workflow.add_edge('RAG', END)
+
+graph = workflow.compile()
+
+## App defination
 
 origins = [
     "https://tangpt.tanflix.me/",
@@ -71,8 +151,33 @@ app.add_middleware(
 
 @app.post("/invoke")
 async def get_response(request : RequestModel):
+
+    
     config = {"configurable": {"session_id": request.session_id}}
     return chain.invoke([HumanMessage(request.message)], config=config)
+
+@app.post("/chat")
+async def get_response(request : RequestModel):
+
+    conversation_history = get_session_history(request.session_id)
+    # Add the user's message to the conversation history
+    conversation_history.append(("user", request.message))
+    
+    
+    #print({'messages': conversation_history})
+    result = graph.invoke({'messages': conversation_history})
+    
+    # Extract the assistant's response 
+    assistant_response = result['messages'][-1].content 
+    
+    #print("Assistant:", assistant_response)
+    
+    # Add the assistant's response to the conversation history
+    conversation_history.append(("assistant", assistant_response))
+
+    return assistant_response
+
+
     
 
 
